1d70f0befadbab98e808aa8305a95655721f6414
Po-Jen Lai <berln579@gmail.com>
2015-02-01 16:54:31 +0800

    start hw1 opencv version

---
+++ hw1/hw1_opencv.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	/*Load two images*/
+
+	/* Compute the motion vector */
+
+
+
+	return 0;
+}
