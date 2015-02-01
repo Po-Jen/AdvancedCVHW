23bf2c11c51ee8f522d067f768065c8e9fdff61f
Po-Jen Lai <berln579@gmail.com>
2015-02-01 19:49:28 +0800

    Added Load image

--- hw1/hw1_opencv.cpp
+++ hw1/hw1_opencv.cpp
@@ -2,12 +2,22 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
+//#define VISUaLIZE
+
 int main(int argc, char *argv[])
 {
 	/*Load two images*/
+	cv::Mat trucka = cv::imread("trucka.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat truckb = cv::imread("truckb.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 
-	/* Compute the motion vector */
+#ifdef  VISUALIZE
+	cv::imshow("truck a", trucka);
+	cv::imshow("truck b", truckb);
+	cv::waitKey();
+#endif
 
+	/* Compute the motion vector by 16x16 Blocks*/
+	
 
 
 	return 0;
