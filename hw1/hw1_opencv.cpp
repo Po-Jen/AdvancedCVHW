94d54523cce477e7c350af992f3ede420bce23be
Po-Jen Lai <berln579@gmail.com>
2015-02-05 16:44:20 +0800

    add border to reference image

--- hw1/hw1_opencv.cpp
+++ hw1/hw1_opencv.cpp
@@ -22,13 +22,24 @@ int main(int argc, char *argv[])
 	cv::imshow("truck b", truckb);
 	cv::waitKey();
 #endif
+
 	/* Add black srroundings to truckb */
 	//search range = 50
-	
+	cv::Mat fatTruckb;
+	int searchRange = 51;
+	int top, bottom, left, right;
+	top = bottom = left = right = (searchRange-1)/2;
+	copyMakeBorder( truckb, fatTruckb, top, bottom, left, right, cv::BORDER_CONSTANT, 0);
+
+#ifdef  VISUALIZE
+	cv::imshow("fat truck b", fatTruckb);
+	cv::waitKey();
+#endif 	
 
 	/* Compute the motion vector by 16x16 Blocks*/
     //Extract all blocks from trucka and save the position of its center
- 		
+ 	int row = trucka.rows;
+ 	int col = trucka.cols;
 
     //Match the location for all blocks in truckb
        
